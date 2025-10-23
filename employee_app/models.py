567bfbbedb651e1cc3f39a4c915f4822e2b046e4
Vvk_0105 <vivekbabu.sics@gmail.com>
2025-10-23 16:15:42 +0530

    model and validation for image size completed

--- employee_app/models.py
+++ employee_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.core.exceptions import ValidationError
 # Create your models here.
+
+def validate_image_size(image):
+    max_size = 5 * 1024 * 1024
+    if image.size > max_size:
+        raise ValidationError("image size should be 5MB.")
+    
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    photo = models.ImageField(upload_to='photos/', blank=True, null=True, validators=[validate_image_size])
+
+    def __str__(self):
+        return self.name
